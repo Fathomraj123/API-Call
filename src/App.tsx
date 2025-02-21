99b3d788e66280c0d5b4987748892cbf9fbf3899
Fathomraj123 <sudarshankumarraj123@gmail.com>
2025-02-21 17:37:48 +0530

    change in to promise

--- src/App.tsx
+++ src/App.tsx
@@ -4,63 +4,61 @@ import { useState } from "react";
 const API_URL = "https://api.thecatapi.com/v1/images/search";
 
 function App() {
-  const [fetchData, setFetchData] = useState<any>(null);
-  const [axiosData, setAxiosData] = useState<any>(null);
+  const [data, setData] = useState(null);
+  const [axiosData, setAxiosData] = useState(null);
 
-  const [loading, setLoading] = useState({
-    fetch: false,
-    axios: false,
-  });
 
-  const getUsingFetch = async () => {
-    setLoading({ ...loading, fetch: true });
-    try {
-      const response = await fetch(API_URL);
-      const data = await response.json();
-      console.log(data);
-      setFetchData(data);
-    } catch (err) {
-      console.error(err);
-    } finally {
-      setLoading({ ...loading, fetch: false });
-    }
+
+
+
+  const getDataUsingFetch = () => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      })
+      .catch((err) => console.error(err));
   };
-  const getUsingAxios = async () => {
-    setLoading({ ...loading, axios: true });
-    try {
-      const response = await axios.get(API_URL);
-      console.log(response.data);
-      setAxiosData(response.data);
-    } catch (err) {
-      console.error(err);
-    } finally {
-      setLoading({ ...loading, axios: false });
-    }
-  };
+  const getDataUsingAxios = () => {
+    axios
+      .get(API_URL)
+      .then((response) => {
+        console.log(response.data);
+        setAxiosData(response.data);
+      })
+      .catch((err) => console.error(err));
+  }
+  
+
   return (
     <>
-      <div className={"container"}>
+      {
+        
+      
+       <div className={"container"}>
         <div>
           <div>
-            {loading.fetch && <p>Loading data using fetch...</p>}
-            <button class="button-64" onClick={getUsingFetch}>Data Using Fetch</button>
+            <h1>Get Data using Fetch</h1>
+            <button class="button-64" onClick={getDataUsingFetch}>Data Using Fetch</button>
           </div>
           <div>
-            <h1>Get Data using Fetch</h1>
-            {fetchData && <pre>{JSON.stringify(fetchData, null, 2)}</pre>}
+            
+            {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
           </div>
         </div>
         <div>
           <div>
-            {loading.axios && <p>Loading data using axios...</p>}
-            <button class="button-64" onClick={getUsingAxios}>Data Using Axios</button>
+            <h1>Get Data using Axios</h1>
+         
+            <button class="button-64" onClick={getDataUsingAxios}>Data Using Axios</button>
           </div>
           <div>
-            <h1>Get Data using Axios</h1>
+           
             {axiosData && <pre>{JSON.stringify(axiosData, null, 2)}</pre>}
           </div>
         </div>
-      </div>
+      </div> }
     </>
   );
 }
